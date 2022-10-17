29a756b547b062c13201545db756695e2cf6e386
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2022-10-17 15:09:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
+from operator import le
 from espn_api.football import League
 import pandas as pd
 import numpy as np
 import requests
 import streamlit as st
 
-st.title("Testing")
+league_id = 115999423
+year = 2022
+
+league = League(league_id, year)
+
+st.text(league.teams)
