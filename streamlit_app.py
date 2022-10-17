94f0ad78c60c142e25609867bb497665ccf6b546
Darren McEwan <github@darrenmcewan.com>
2022-10-17 16:53:15 -0700

    changed week

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ st.subheader(f"{league.year} Season")
 max_week = league.current_week
 week = st.slider("Select NFL Week to view", 1,max_week, max_week)
 
-def team_scores(league, max_week):
+def team_scores(league, week):
     teams = defaultdict()
     for team in league.teams:
-            teams[team.team_name] = team.scores[:max_week-1]
+            teams[team.team_name] = team.scores[:week-1]
     return dict(teams)
 
-scores = team_scores(league, max_week)
+scores = team_scores(league, week)
 
 df = pd.DataFrame.from_dict(scores)
 st.line_chart(df)
