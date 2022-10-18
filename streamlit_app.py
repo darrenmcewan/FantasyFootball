e170b3681acdc0f02b8fa97fcb836e1ff112a378
Darren McEwan <github@darrenmcewan.com>
2022-10-17 17:59:49 -0700

    power_

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,15 @@ st.subheader(f"{league.year} Season")
 max_week = league.current_week-1
 week = st.slider("Select NFL Week to view", 1,max_week, max_week)
 
-def team_scores(league, week):
+def team_scores(league, week, type):
     teams = defaultdict()
     for team in league.teams:
+        if type == "scores":
             teams[team.team_name] = team.scores[:week]
     return dict(teams)
 
 scores = team_scores(league, week)
+
 
 df = pd.DataFrame.from_dict(scores)
 df.index = df.index+1
@@ -39,3 +41,24 @@ fig.update_yaxes(showgrid=False)
 st.plotly_chart(fig)
 
 
+def power_rankings(league, max_week):
+    dict_1=dict()
+    for i in range(max_week):
+      power_rankings = league.power_rankings(i)
+      
+      for ranking,team in power_rankings:
+        dict_1.setdefault(team, []).append(ranking)
+    return dict_1
+
+scores = power_rankings(league, max_week)
+df = pd.DataFrame.from_dict(scores)
+df.index = df.index+1
+fig = px.line(df)
+fig.update_layout(
+    title="Scores Through the Weeks",
+    xaxis_title="Week Num",
+    yaxis_title="Points Scored",
+    legend_title="Team",
+)
+fig.update_yaxes(showgrid=False)
+st.plotly_chart(fig)
