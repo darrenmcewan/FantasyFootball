7c9049d3369c1e61d2229be1b739c337c7f3d868
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2022-11-16 10:04:42 -0800

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 import plotly.express as px
 from statistics import variance
 
-
 league_id = 115999423
 year = 2022
 
@@ -15,29 +14,32 @@ league = League(league_id, year)
 league.refresh()
 
 best_matchup = 100
-ind=0
+ind = 0
 for i, match in enumerate(league.scoreboard()):
-  matchupVar = variance([match.home_team.standing, match.away_team.standing])
-  print(matchupVar)
-  if matchupVar < best_matchup:
-    best_matchup = matchupVar
-    ind=i
+    matchupVar = variance([match.home_team.standing, match.away_team.standing])
+    print(matchupVar)
+    if matchupVar < best_matchup:
+        best_matchup = matchupVar
+        ind = i
 
 matchup_week = league.scoreboard()[ind]
 
 st.title("Sunnyvale Ballers")
 st.subheader(f"{league.year} Season Overview")
 st.markdown(f'### Current NFL Week: {league.current_week}', unsafe_allow_html=False)
-st.markdown(f'#### This weeks matchup of the week is {matchup_week.home_team.team_name} ({matchup_week.home_team.standing}) vs {matchup_week.away_team.team_name} ({matchup_week.away_team.standing})')
+st.markdown(
+    f'#### This weeks matchup of the week is {matchup_week.home_team.team_name} ({matchup_week.home_team.standing}) vs {matchup_week.away_team.team_name} ({matchup_week.away_team.standing})')
 projected_winner = league.box_scores()[ind].home_projected > league.box_scores()[ind].away_projected
 if projected_winner:
-  st.markdown(f"#### The home team {league.box_scores()[ind].home_team.team_name} is expected to win with {league.box_scores()[ind].home_projected}")
+    st.markdown(
+        f"#### The home team {league.box_scores()[ind].home_team.team_name} is expected to win with {league.box_scores()[ind].home_projected}")
 else:
-  st.markdown(f"#### The away team {league.box_scores()[ind].away_team.team_name} is expected to win with {league.box_scores()[ind].away_projected}")
+    st.markdown(
+        f"#### The away team {league.box_scores()[ind].away_team.team_name} is expected to win with {league.box_scores()[ind].away_projected}")
 
+max_week = league.current_week - 1
+week = st.slider("Select NFL Week to view", 1, max_week, max_week)
 
-max_week = league.current_week-1
-week = st.slider("Select NFL Week to view", 1,max_week, max_week)
 
 def team_scores(league, week):
     teams = defaultdict()
@@ -45,13 +47,15 @@ def team_scores(league, week):
         teams[team.team_name] = team.scores[:week]
     return dict(teams)
 
+
 def power_rankings(league, week):
-    dict_1=dict()
+    dict_1 = dict()
     for i in range(week):
-      power_rankings = league.power_rankings(i)
-      for ranking,team in power_rankings:
-        dict_1.setdefault(team, []).append(ranking)
+        power_rankings = league.power_rankings(i)
+        for ranking, team in power_rankings:
+            dict_1.setdefault(team, []).append(ranking)
     return dict_1
+
 
 def melt(df, col_vals, key, value):
     assert type(df) is pd.DataFrame
@@ -63,13 +67,13 @@ def melt(df, col_vals, key, value):
         melted_c[value] = df[c]
         melted_sections.append(melted_c)
     melted = pd.concat(melted_sections)
-    
+
     return melted
 
 
 scores = team_scores(league, week)
 df = pd.DataFrame.from_dict(scores)
-df.index = df.index+1
+df.index = df.index + 1
 
 fig = px.line(df)
 fig.update_layout(
@@ -81,20 +85,16 @@ fig.update_layout(
 fig.update_yaxes(showgrid=False)
 st.plotly_chart(fig)
 
-
-
 ##power ranking
 power_ranks = power_rankings(league, week)
 df_pr = pd.DataFrame.from_dict(power_ranks)
 df_pr.iloc[:, 0:] = df_pr.iloc[:, 0:].astype(str).astype(float)
-df_pr.index = df_pr.index+1
+df_pr.index = df_pr.index + 1
 df_pr.reset_index(inplace=True)
-df_pr = df_pr.rename(columns = {'index':'Week'})
+df_pr = df_pr.rename(columns={'index': 'Week'})
 
 melted = melt(df_pr, df_pr.iloc[:, 1:], key='Team', value='Power Ranking')
 melted_df = melted[['Week', 'Team', 'Power Ranking']]
-
-
 
 fig_pr = px.line(melted_df, x='Week', y='Power Ranking', color='Team')
 fig_pr.update_layout(
@@ -106,15 +106,19 @@ fig_pr.update_layout(
 fig_pr.update_yaxes(showgrid=False)
 st.plotly_chart(fig_pr)
 
-
-colnames = ['Player', 'Percent_owned', 'percent_started', 'projected_points', 'position rank', 'position', 'current week opponent', 'opp rank']
+colnames = ['Player', 'Percent_owned', 'percent_started', 'projected_points', 'position rank', 'position',
+            'current week opponent', 'opp rank']
 positions = ['QB', 'WR', 'RB', 'TE', 'K']
 
 free_agents = list()
 
 for i in positions:
-  for free_agent in league.free_agents(7, 50, position=i):
-    free_agents.append((free_agent.name, free_agent.percent_owned, free_agent.percent_started, free_agent.projected_points, free_agent.posRank, free_agent.position, free_agent.pro_opponent, free_agent.pro_pos_rank))
+    for free_agent in league.free_agents(7, 50, position=i):
+        free_agents.append((free_agent.name, free_agent.percent_owned, free_agent.percent_started,
+                            free_agent.projected_points, free_agent.posRank, free_agent.position,
+                            free_agent.pro_opponent, free_agent.pro_pos_rank))
 
-df = pd.DataFrame(free_agents, columns = colnames)
+df = pd.DataFrame(free_agents, columns=colnames)
 st.dataframe(df)
+
+st.text("This is a test")
